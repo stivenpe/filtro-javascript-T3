2562f1a16b18ca357e6629e47634027c6d5f0463
stivenpe <perezjeisson003@gmail.com>
2024-10-03 17:08:18 -0500

    ajutes general

---
+++ script.js
@@ -0,0 +1,6 @@
+fetch("C:\Users\jeisson\Desktop\filtro-js\filtro-javascript-T3\data.json")
+.then((response) =>response.json)
+const heroeslist = document.getElementById('heroes-list');
+const heroesimg = document.getElementById('heroes-img');
+
+``

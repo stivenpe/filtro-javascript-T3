a5c3403e69bd6d1e4432463a216c8f03418bff40
stivenpe <perezjeisson003@gmail.com>
2024-10-03 17:50:04 -0500

    ajutes

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-fetch("C:\Users\jeisson\Desktop\filtro-js\filtro-javascript-T3\data.json")
-.then((response) =>response.json)
-const heroeslist = document.getElementById('heroes-list');
-const heroesimg = document.getElementById('heroes-img');
 
-``
+document.addEventListener("DOMContentLoaded", function() {
+
+    fetch('C:\Users\jeisson\Desktop\filtro-js\filtro-javascript-T3\data.json')
+
+    .then(response => response.json())
+    
+    .then(data => {
+
+    const heroesInfoDiv = document.getElementById('heroes-info');
+   
+    heroesInfoDiv.innerHTML = `
+    <h2>${data.name}</h2>
+    <p><strong>personaje:</strong> ${data.personaje}</p>
+    <p><strong>estudios:</strong> ${data.estudios}</p>
+    <p><strong>nombre:</strong> ${data.nombre}</p>
+    <p><strong>habilidades:</strong> ${data.habilidades}</p>
+    <p><strong>aparicion:</strong> ${data.aparicion}</p>
+    <p><strong>:</strong> ${data.img}</p>
+    <h3>:</h3>
+    <ul>
+    <li><a href="${data.links.img}" target="_blank">img</a></li>
+    
+    </ul>
+    `;
+    })
+    
+    .catch(error => {
+    console.error('Error al obtener los datos:', error);
+    });
+});
